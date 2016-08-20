102c25ba33ef1eb86fa0560b332f19fa42e9aab1
stone1255 <awjohnson12@hotmail.com>
2016-08-20 11:54:46 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     $('.button').click(function(){
-     $('img').fadeOut(1000);
+     $('img').fadeOut('slow');
   });
     $('#button-two').click(function(){
-     $('img').fadeIn(1000);
+     $('img').fadeIn('slow');
   });
 });
