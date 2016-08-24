bb91473cc827f951a825bc3c73cef45b6d82ad00
stone1255 <awjohnson12@hotmail.com>
2016-08-23 19:58:30 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
  });
  
  $('#fancy-text').click(function(){
-  $(this).css({"font-style": "italic", "color":"puruple", "font-size":"30px"});
+  $(this).css({"font-style":"italic", "color":"purple", "font-size":"30px"});
  })
  
 });
