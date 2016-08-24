6e9eb995de305ff4c84e35967e41a3878f458c8a
stone1255 <awjohnson12@hotmail.com>
2016-08-23 20:04:56 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
  $('p').fadeIn(2000);
  
  $('header').click(function(){
-  $('header').css("background-color", "blue");
+  $(this).css("background-color", "blue");
  });
  
  $('#fancy-text').click(function(){
